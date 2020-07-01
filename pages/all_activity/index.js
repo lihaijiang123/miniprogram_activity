5819adb00dd3a2546c967ccf285fc9c45010b9d7
Haijiang Li <haijiang.li@mdpi.com>
2020-07-02 00:27:29 +0800

    ok

--- pages/all_activity/index.js
+++ pages/all_activity/index.js
@@ -29,45 +29,92 @@ Page({
 		title3: '价格',
 		title4: '活动类别',
 		//default value
-		value1: 0,
-		value2: '10',
-    value3: '20',
-		value4: '30',
+		value: 0,
+
+		
 		// option
 		option1: [
-      { text: '全部', value: 0 },
-      { text: '今天', value: 1 },
-			{ text: '明天', value: 2 },
-      { text: '本周', value: 3 },
-      { text: '本月', value: 4 },
-      { text: '本年', value: 5 },
-      { text: '明年及以后', value: 6 },
-      { text: '已结束', value: 7 },
+      { text: '全部', value: '1' },
+      { text: '今天', value: '2' },
+			{ text: '明天', value: '3' },
+      { text: '本周', value: '4' },
+      { text: '本月', value: '5' },
+      { text: '本年', value: '6' },
+      { text: '明年及以后', value: '7' },
+      { text: '已结束', value: '8' },
     ],
     option2: [
-      { text: '最多浏览', value: '10' },
-      { text: '最多收藏', value: '11' },
-			{ text: '最多分享', value: '12' },
-			{ text: '最新发布', value: '12' },
+      { text: '最多浏览', value: '101' },
+      { text: '最多收藏', value: '102' },
+			{ text: '最多分享', value: '103' },
+			{ text: '最新发布', value: '104' },
 		],
 		option3: [
-      { text: '全部', value: '20' },
-      { text: '免费', value: '21' },
-      { text: '收费', value: '22' },
+      { text: '全部', value: '201' },
+      { text: '免费', value: '202' },
+      { text: '收费', value: '203' },
 		],
-		option4: [
-      { text: '活动类别', value: '30' },
-      { text: '好评排序', value: '31' },
-      { text: '销量排序', value: '32' },
-    ],
-		switchTitle1: '包邮',
+	
 
 	},
 
-  onSwitch1Change({ detail }) {
-    this.setData({ switch1: detail });
-  },
+	// 下拉框前三个
+	change: function({detail}) {
+		let url = app.globalData.dev_api;
+		let that = this;
+		wx.request({
+			url: url + '/test',
+			data: {
+				userId: wx.getStorageSync('user_id'),
+				city: that.data.city,
+				type: 'list',
+				flag: detail
+			},
+			success: function (res) {
+				let data = res.data.data;
+				data.active_list.forEach(item => {
+					item.serve_type = item.serve_type.split(',')
+				})
+				that.setData({
+					list_data: data,
+					value: detail,
 
+				})
+			}
+		})
+
+	},
+
+	// 下拉框最后一个
+	dropdownClick: function(event) {
+		let that = this;
+		let url = app.globalData.dev_api;
+		let flag = event.currentTarget.dataset.flag;
+		let id = event.currentTarget.dataset.id;
+		let detail = flag + '-' + id;
+
+		this.selectComponent('#item').toggle();
+		
+		wx.request({
+			url: url + '/test',
+			data: {
+				userId: wx.getStorageSync('user_id'),
+				city: that.data.city,
+				type: 'list',
+				flag: detail
+			},
+			success: function (res) {
+				let data = res.data.data;
+				data.active_list.forEach(item => {
+					item.serve_type = item.serve_type.split(',')
+				})
+				that.setData({
+					list_data: data,
+					value: detail,
+				})
+			}
+		})
+	},
 
 	// goCity
 	goCity: function () {
@@ -83,55 +130,6 @@ Page({
 		})
 	},
 
-	// setTabIndex
-	setTabIndex: function (e) {
-		let index = e.currentTarget.dataset.index;
-		this.setData({
-			tab_active: index,
-			is_options: false,
-			options_index: 0,
-		})
-		wx.pageScrollTo({
-			scrollTop: 0,
-			duration: 300,
-		})
-		let that = this;
-		setTimeout(() => {
-			let url = app.globalData.dev_api;
-			wx.request({
-				url: url + '/test',
-				data: {
-					userId: wx.getStorageSync('user_id'),
-					city: that.data.city,
-					serve_type_id: that.data.tab_active,
-					type: 'list',
-				},
-				success: function (res) {
-					let data = res.data.data;
-					if (data.active_list.length <= 0) {
-						that.setData({
-							no: true,
-						})
-					}else {
-						that.setData({
-							no: false,
-						})
-					}
-					data.active_list.forEach(item => {
-						item.serve_type = item.serve_type.split(',')
-					})
-					that.setData({
-						list_data: data,
-						begin_time__desc: 0,
-						hold_mode: '',
-						page: 1,
-						sj: '',
-						cs: '',
-					})
-				}
-			})
-		}, 300)
-	},
 
 	// goDetail
 	goDetail: function (e) {
@@ -140,6 +138,7 @@ Page({
 			url: '/pages/content/index?id=' + id,
 		})
 	},
+
 
 	// pageInit
 	pageInit: function () {
@@ -163,119 +162,6 @@ Page({
 			}
 		})
 	},
-
-	// clickHandler
-	// clickHandler: function (e) {
-	// 	let index = e.currentTarget.dataset.index;
-	// 	this.setData({
-	// 		options_index: index,
-	// 		is_options: true,
-	// 	})
-	// },
-
-	// delHandler
-	// delHandler: function () {
-	// 	this.setData({
-	// 		options_index: 0,
-	// 		is_options: false,
-	// 	})
-	// },
-
-	// clickShJi
-	// clickShJi: function (e) {
-	// 	let index = e.currentTarget.dataset.index;
-	// 	let url = app.globalData.dev_api;
-	// 	let that = this;
-	// 	wx.pageScrollTo({
-	// 		scrollTop: 0,
-	// 		duration: 300,
-	// 	})
-	// 	setTimeout(() => {
-	// 		wx.request({
-	// 			url: url + '/search',
-	// 			data: {
-	// 				userId: 1,
-	// 				hold_mode: that.data.hold_mode,
-	// 				order: index,
-	// 				serve_type_id: that.data.tab_active
-	// 			},
-	// 			success: function (res) {
-	// 				let data = res.data.data;
-	// 				data.active_list.data.forEach(item => {
-	// 					item.serve_type = item.serve_type.split(',')
-	// 				})
-	// 				that.setData({
-	// 					list_data: data,
-	// 					begin_time__desc: index,
-	// 					is_options: false,
-	// 					options_index: 0,
-	// 					page: 1,
-	// 				})
-	// 				if (that.data.begin_time__desc == 0) {
-	// 					that.setData({
-	// 						sj: ''
-	// 					})
-	// 				} else if (that.data.begin_time__desc == 'begin_time__asc') {
-	// 					that.setData({
-	// 						sj: '升序'
-	// 					})
-	// 				} else if (that.data.begin_time__desc == 'begin_time__desc') {
-	// 					that.setData({
-	// 						sj: '降序'
-	// 					})
-	// 				}
-	// 			}
-	// 		})
-	// 	}, 300)
-	// },
-
-	// clickXsX
-	// clickXsX: function (e) {
-	// 	let index = e.currentTarget.dataset.index;
-	// 	let url = app.globalData.dev_api;
-	// 	let that = this;
-	// 	wx.pageScrollTo({
-	// 		scrollTop: 0,
-	// 		duration: 300,
-	// 	})
-	// 	setTimeout(() => {
-	// 		wx.request({
-	// 			url: url + '/search',
-	// 			data: {
-	// 				userId: 1,
-	// 				hold_mode: index,
-	// 				order: that.data.begin_time__desc,
-	// 				serve_type_id: that.data.tab_active
-	// 			},
-	// 			success: function (res) {
-	// 				let data = res.data.data;
-	// 				data.active_list.data.forEach(item => {
-	// 					item.serve_type = item.serve_type.split(',')
-	// 				})
-	// 				that.setData({
-	// 					list_data: data,
-	// 					hold_mode: index,
-	// 					is_options: false,
-	// 					options_index: 0,
-	// 					page: 1,
-	// 				})
-	// 				if (that.data.hold_mode == 0) {
-	// 					that.setData({
-	// 						cs: ''
-	// 					})
-	// 				} else if (that.data.hold_mode == '1') {
-	// 					that.setData({
-	// 						cs: '线上'
-	// 					})
-	// 				} else if (that.data.hold_mode == '2') {
-	// 					that.setData({
-	// 						cs: '线下'
-	// 					})
-	// 				}
-	// 			}
-	// 		})
-	// 	}, 300)
-	// },
 
 	/**
 	 * 生命周期函数--监听页面加载
@@ -374,7 +260,7 @@ Page({
 	onReachBottom: function () {
 		let url = app.globalData.dev_api;
 		let page = this.data.page;
-		let serve_type_id = this.data.tab_active || '';
+		
 		let that = this;
 		page++
 		wx.request({
@@ -382,11 +268,10 @@ Page({
 			data: {
 				userId: wx.getStorageSync('user_id'),
 				city: that.data.city,
-				hold_mode: that.data.hold_mode,
-				order: that.data.begin_time__desc,
 				serve_type_id: serve_type_id,
 				page: page,
 				type: 'list',
+				flag: that.value
 			},
 			success: function (res) {
 				if (res.data.data.active_list.length <= 0) {
